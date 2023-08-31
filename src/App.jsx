66d6512595aed08dcadd975b448495709cd0daab
aldyvictor <aldyvictor1046@gmail.com>
2023-08-31 23:14:12 +0700

    fix head: add meta and favicon

--- src/App.jsx
+++ src/App.jsx
@@ -17,34 +17,55 @@ import {
   Stats,
   Testimonials,
 } from "./components/Index";
+import { fav24 } from "./assets";
+import DocumentMeta from "react-document-meta";
+import Favicon from "react-favicon";
 
-const App = () => (
-  <div className="bg-primary w-full overflow-hidden">
-    <div className={`${styles.paddingX} ${styles.flexCenter}`}>
-      <div className={`${styles.boxWidth}`}>
-        <Navbar />
-      </div>
-    </div>
+const App = () => {
+  const meta = {
+    title: "Hoobank | Designed by One Week Wonders",
+    description: "A new way to make the payments easy, reliable and secure.",
+    meta: {
+      charset: "utf-8",
+      name: {
+        keywords: "Hoobank, Banking, Payment, Finance, Business, Money",
+      },
+    },
+  };
 
-    <div className={`bg-primary ${styles.flexStart}`}>
-      <div className={`${styles.boxWidth}`}>
-        <Hero />
-      </div>
-    </div>
+  return (
+    <>
+      <DocumentMeta {...meta} />
+      <Favicon url={fav24} />
 
-    <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
-      <div className={`${styles.boxWidth}`}>
-        <Stats />
-        <Business />
-        <Billing />
-        <CardDeal />
-        <Testimonials />
-        <Clients />
-        <CTA />
-        <Footer />
+      <div className="bg-primary w-full overflow-hidden">
+        <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+          <div className={`${styles.boxWidth}`}>
+            <Navbar />
+          </div>
+        </div>
+
+        <div className={`bg-primary ${styles.flexStart}`}>
+          <div className={`${styles.boxWidth}`}>
+            <Hero />
+          </div>
+        </div>
+
+        <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+          <div className={`${styles.boxWidth}`}>
+            <Stats />
+            <Business />
+            <Billing />
+            <CardDeal />
+            <Testimonials />
+            <Clients />
+            <CTA />
+            <Footer />
+          </div>
+        </div>
       </div>
-    </div>
-  </div>
-);
+    </>
+  );
+};
 
 export default App;
